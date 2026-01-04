6946335d5e3f633af27f91b7b7d361cb1c0b82f3
lucas09986 <luscamotagp@gmail.com>
2026-01-04 19:38:28 -0300

    Adicionando verificação simples de login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title> Orbit Help Desk</title>
+    <title>Orbit Help Desk</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -32,12 +32,27 @@
               Login
             </div>
             <div class="card-body">
-              <form>
+              <form action="valida_login.php" method="post">
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+                  <input name="senha" type="password" class="form-control" placeholder="Senha">
+                </div>
+
+                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro'){?>
+
+                <div class="text-danger">
+                  Usuário ou senha inválido(s).
+                </div>
+                
+                
+
+               <?php } ?>
+               <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2')?>
+
+                <div class="text-danger">
+                  Faça login antes de acessar as páginas protegidas.
                 </div>
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
